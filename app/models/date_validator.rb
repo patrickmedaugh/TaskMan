704ae66cff4624d06648839534d2409cd0ff9ded
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-08-11 13:34:04 -0600

    Date validation completed

--- app/models/date_validator.rb
+++ app/models/date_validator.rb
@@ -1,7 +1,6 @@
 class DateValidator < ActiveModel::Validator
   def validate(record)
-    puts "Here is your record=========\n #{record}"
-    if record.due_date < record.start_date
+    if record.due_date < record.start_date || record.start_date < DateTime.now.to_date
       record.errors[:base] << "Invalid dates"
     end
   end
