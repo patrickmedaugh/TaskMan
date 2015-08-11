2fe754aa66ac0ec68265b3cdf358159b1a97764b
Patrick Medaugh <patrickmedaugh@gmail.com>
2015-08-11 10:14:20 -0600

    new task template

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,21 @@
 class TasksController < ApplicationController
   def new
+    @task = Task.new
+  end
 
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to list_path(@task.list_id)
+    else
+      render :new
+      flash[:error] = "Invalid Task"
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :due_date, :start_date, :status, :list_id)
   end
 end
